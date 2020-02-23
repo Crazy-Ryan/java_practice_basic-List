784145d506d3a776f60d35fe0437d372959d4f89
Ryan.Wang <ranwang1995@outlook.com>
2020-02-24 00:53:17 +0800

    [WangRan] feat: finish Practice1

--- collection/src/main/java/com/thoughtworks/collection/Practice1.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice1.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.collection;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class Practice1 {
     public static void main(String[] args) {
@@ -8,9 +11,16 @@ public class Practice1 {
     }
 
     /**
-     *  生成10个0到20的随机整数，要求不能重复
+     * 生成10个0到20的随机整数，要求不能重复
      */
     public static Collection<Integer> randomNumber() {
-        return null;
+
+        Random random = new Random();
+        Set<Integer> set = new HashSet<>();
+        while (set.size() != 10) {
+            set.add(random.nextInt(21));
+        }
+
+        return set;
     }
 }
